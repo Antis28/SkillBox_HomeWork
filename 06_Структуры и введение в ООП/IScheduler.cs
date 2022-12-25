3870724fb72c80962b5196b5644a87f7332eda07
Antis <Antis28@mail.ru>
2022-12-25 11:25:05 +0300

    add интерфейс для ежедневника

---
+++ 06_Структуры и введение в ООП/IScheduler.cs
@@ -0,0 +1,19 @@
+﻿namespace _06_Структуры_и_введение_в_ООП;
+
+internal interface IScheduler
+{
+    public int Count { get; }
+    
+    public void AddRecord(RecordElement record);
+    public void RemoveRecord(RecordElement record);
+    public void RemoveRecord(int index);
+    public void EditRecord(int index, RecordElement record);
+
+    public void Save();
+    public void Load();
+
+    public RecordElement GetRecord(int index);
+    public void AppendFromFile();
+    public void SelectByDate();
+    public void SortByField();
+}
