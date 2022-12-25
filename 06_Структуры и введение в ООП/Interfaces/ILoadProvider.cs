b21b6d1a45d20e36a35fda9424ab330a5d694ec5
Antis <Antis28@mail.ru>
2022-12-25 16:13:55 +0300

    Тест ежедневника

---
+++ 06_Структуры и введение в ООП/Interfaces/ILoadProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace _06_Структуры_и_введение_в_ООП;
+
+public interface ILoadProvider
+{
+    public List<RecordElement> LoadFromFile();
+}
+
--- 06_Структуры и введение в ООП/Scheduler.cs
+++ 06_Структуры и введение в ООП/Scheduler.cs
@@ -1,123 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace _06_Структуры_и_введение_в_ООП
 {
-    internal struct Scheduler : IScheduler
+    public struct Scheduler : IScheduler
     {
         public int Count => _records.Count;
-
-        private string _path;
         private List<RecordElement> _records;
-        private Encoding _defaultEncoding;
 
-        public Scheduler(string path)
+        private readonly ISaveProvider _saveProvider;
+        private readonly ILoadProvider _loadProvider;
+
+        public Scheduler(string pathToFile)
         {
-            _path = path;
-            _path = "db.csv";
-            _defaultEncoding = Encoding.UTF8;
-            _records = new List<RecordElement>();
-            _records = LoadFromFile(_path);
+            pathToFile = "db.csv";
+            _loadProvider = new LoadProvider(pathToFile, Encoding.UTF8);
+            _saveProvider = new SaveProvider(pathToFile, Encoding.UTF8);
         }
 
-        public readonly void AddRecord(RecordElement record) 
+        /// <summary>
+        /// Для тестов
+        /// </summary>
+        /// <param name="loadProvider"></param>
+        /// <param name="saveProvider"></param>
+        public Scheduler(ILoadProvider loadProvider, ISaveProvider saveProvider)
+        {
+            _loadProvider = loadProvider;
+            _saveProvider = saveProvider;
+        }
+
+        public readonly void AddRecord(RecordElement record)
         {
             _records.Add(record);
         }
 
-        public void RemoveRecord(RecordElement record) 
+        public void RemoveRecord(RecordElement record)
         {
             _records.Remove(record);
         }
+
         public void RemoveRecord(int index)
         {
             _records.RemoveAt(index);
         }
 
 
-        public void EditRecord(int index, RecordElement record) 
+        public void EditRecord(int index, RecordElement record)
         {
             _records[index] = record;
         }
+
         public void Save()
         {
-            SaveToFile();
+            _saveProvider.SaveToFile(_records);
         }
 
-        public void Load() { LoadFromFile(_path); }
-        public void AppendFromFile() { throw new NotImplementedException(); }
-        public void SelectByDate() { throw new NotImplementedException(); }
-        public void SortByField() { throw new NotImplementedException(); }
+
+        public void Load()
+        {
+            _records = _loadProvider.LoadFromFile();
+        }
+
+        public void AppendFromFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SelectByDate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SortByField()
+        {
+            throw new NotImplementedException();
+        }
 
         public RecordElement GetRecord(int index)
         {
             return _records[index];
-        }       
-        
-        private void SaveToFile()
-        {
-            var note = new StringBuilder();
-
-            foreach (var record in _records)
-            {
-                note.AppendLine(PrepareFormat(record));
-            }
-            using (StreamWriter sw = new StreamWriter(_path, false, _defaultEncoding))
-            {
-                sw.Write(note);
-            }
         }
-
-        private string PrepareFormat(RecordElement record)
-        {
-            var note = new StringBuilder();
-            // Дата записи               
-            note.Append($"{record.Date}\t");
-            // Дата окончания               
-            note.Append($"{record.EndDate}\t");
-            // Место проведения             
-            note.Append($"{record.Place}\t");
-            // Заголовок записи
-            note.Append($"{record.Title}\t");
-            // Текст записи
-            note.Append($"{record.Text}\t");
-
-            return note.ToString();
-        }
-
-        private List<RecordElement> LoadFromFile(string path)
-        {
-            List<RecordElement> recordElements = new List<RecordElement>();
-            if (!File.Exists(_path))
-            {
-                return recordElements;
-            }
-            
-            // чтение из файла
-            using (var sr = new StreamReader(_path, _defaultEncoding))
-            {
-                string line;
-                while ((line = sr.ReadLine()) != null)
-                {
-                    string[] data = line.Split('\t');
-                    Console.WriteLine($"{data[0],15}{data[1],8} {data[2]}");
-                    var rec = new RecordElement
-                    {
-                        Date = DateTime.Parse(data[0]),
-                        EndDate = DateTime.Parse(data[1]),
-                        Place = data[2],
-                        Title = data[3],
-                        Text = data[4],
-                    };
-                    recordElements.Add(rec);
-                }
-            }
-            return recordElements;
-        }
-       
     }
 }
