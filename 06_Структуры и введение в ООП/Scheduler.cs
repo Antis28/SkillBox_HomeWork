aa8618007deb47300a754f0c1fb6d0e81c3b26c1
Antis <Antis28@mail.ru>
2022-12-25 19:20:19 +0300

    feature SortByField + test

--- 06_Структуры и введение в ООП/Scheduler.cs
+++ 06_Структуры и введение в ООП/Scheduler.cs
@@ -59,11 +59,68 @@ namespace _06_Структуры_и_введение_в_ООП
             return res.ToList();
         }
 
-        public void SortByField()
+        public List<RecordElement> SortByField(RecordField field)
         {
-            throw new NotImplementedException();
+            switch (field)
+            {
+                case RecordField.Date:
+                    return SortByDate();
+                    break;
+                case RecordField.EndDate:
+                    return SortByEndDate();
+                    break;
+                case RecordField.Place:
+                    return SortByPlace();
+                    break;
+                case RecordField.Title:
+                    return SortByTitle();
+                    break;
+                case RecordField.Text:
+                    return SortByText();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(field), field, null);
+            }
         }
 
+        
+
         public RecordElement GetRecord(int index) => _records[index];
+
+        private List<RecordElement> SortByDate()
+        {
+            var res = from rec in _records
+                      orderby rec.Date, rec.Title
+                      select rec;
+            return res.ToList();
+        }
+        private List<RecordElement> SortByTitle()
+        {
+            var res = from rec in _records
+                      orderby rec.Title
+                      select rec;
+            return res.ToList();
+        }
+        private List<RecordElement> SortByText()
+        {
+            var res = from rec in _records
+                      orderby rec.Text
+                      select rec;
+            return res.ToList();
+        }
+        private List<RecordElement> SortByPlace()
+        {
+            var res = from rec in _records
+                      orderby rec.Place
+                      select rec;
+            return res.ToList();
+        }
+        private List<RecordElement> SortByEndDate()
+        {
+            var res = from rec in _records
+                      orderby rec.EndDate
+                      select rec;
+            return res.ToList();
+        }
     }
 }
